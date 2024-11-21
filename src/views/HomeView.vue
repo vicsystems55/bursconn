7574aa55a5535d0c6ef10e29abdba9369701d7fc
vicsystems <victorasuquob@gmail.com>
2024-11-21 14:40:31 +0100

    bursconn update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -90,8 +90,8 @@
     <div class="header-two py-5  animate__animated animate__fadeIn text-center">
       <div class="container font-header animate__animated animate__fadeInUp">
         <h2>A 4 DAY ANNUAL NATIONAL WORKSHOP ON PUBLIC FINANCE MANAGEMENT
-          AND LEADERSHIP STRATEGY FOR RECTORS, PROVOSTS AND BURSARS OF COLLEGES OF TECHNOLOGY, EDUCATION,
-          HEALTH AND AGRICULTURE, IN NIGERIA
+          AND LEADERSHIP STRATEGY FOR RECTORS, PROVOSTS, BURSARS OF COLLEGES OF TECHNOLOGY, EDUCATION,
+          HEALTH, AGRICULTURE AND ANY SENIOR MANAGEMENT STAFF OF POLYTECHNICS IN NIGERIA
         </h2>
         <h2 style="color: yellow" class="py-2">Reducing cost and delivering result</h2>
         <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
